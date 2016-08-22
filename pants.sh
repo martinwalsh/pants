b7a00390ad6f4001a040dae493939d5d71388f62
Martin Walsh <github@mwalsh.org>
2016-08-21 21:20:40 -0500

    use did_update to determine if interpreters should be installed

--- pants.sh
+++ pants.sh
@@ -11,35 +11,43 @@ register 'types/fragment.sh'
 ok brew
 ok brew bork
 ok brew git
-ok git ~/.pants ${PANTS} --branch=${PANTS_BRANCH}
-ok directory ~/bin
-ok file ~/bin/pants files/pants --permissions=755
+ok git ${HOME}/.pants ${PANTS} --branch=${PANTS_BRANCH}
+ok directory ${HOME}/bin
+ok file ${HOME}/bin/pants files/pants --permissions=755
 
 ## NODE ENVIRONMENT
 ok brew nvm
-ok directory ~/.nvm
-ok fragment fragments/nvm.profile ~/.profile
-source ~/.profile
-for PANTS_NODE_VERSION in $PANTS_NODE_VERSIONS; do
-  nvm install $PANTS_NODE_VERSION
-done
+if did_update; then
+  ok directory ${HOME}/.nvm
+  ok fragment fragments/nvm.profile ${HOME}/.profile
+  source fragments/nvm.profilE
+  for PANTS_NODE_VERSION in $PANTS_NODE_VERSIONS; do
+    nvm install $PANTS_NODE_VERSION
+  done
+fi
 
 ## RUBY ENVIRONMENT
 ok brew rbenv
-ok brew ruby-build
-ok fragment fragments/rbenv.profile ~/.profile
-source ~/.profile
-for PANTS_RUBY_VERSION in $PANTS_RUBY_VERSIONS; do
-  rbenv install --skip-existing ${PANTS_RUBY_VERSION}
-done
+if did_update; then
+  ok brew ruby-build
+  if did_update; then
+    ok fragment fragments/rbenv.profile ${HOME}/.profile
+    source fragments/rbenv.profile
+    for PANTS_RUBY_VERSION in $PANTS_RUBY_VERSIONS; do
+      rbenv install --skip-existing ${PANTS_RUBY_VERSION}
+    done
+  fi
+fi
 
 ## PYTHON ENVIRONMENT
 ok brew pyenv
-ok fragment fragments/pyenv.profile ~/.profile
-source ~/.profile
-for python_version in $PANTS_PYTHON_VERSIONS; do
-  pyenv install --skip-existing $python_version
-  pyenv shell $python_version
-  ok pip ipython
-done
-pyenv shell --unset
+if did_update; then
+  ok fragment fragments/pyenv.profile ${HOME}/.profile
+  source fragments/pyenv.profile
+  for python_version in $PANTS_PYTHON_VERSIONS; do
+    pyenv install --skip-existing $python_version
+    pyenv shell $python_version
+    ok pip ipython
+  done
+  pyenv shell --unset
+fi
